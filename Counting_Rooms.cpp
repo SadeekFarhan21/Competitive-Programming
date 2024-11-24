7149c6bc29cf253063a1fc7aca94eef19f5d76e0
Farhan Sadeek <farhansadeek019@gmail.com>
2024-11-24 10:43:05 -0500

    counting rooms

--- Counting_Rooms.cpp
+++ Counting_Rooms.cpp
@@ -4,14 +4,14 @@ using namespace std;
 const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, 1, 0, -1};
 
-void dfs(vector<vector<char> > &matrix, vector<vector<bool> > &visited, int x, int y, int a, int b)
+void dfs(vector<vector<char>> &matrix, vector<vector<bool>> &visited, int x, int y, int a, int b)
 {
     visited[x][y] = true;
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if (nx >= 0 && nx < a && ny > 0 && ny < b && matrix[nx][ny] == '.' && !visited[nx][ny])
+        if (nx >= 0 && nx < a && ny >= 0 && ny < b && matrix[nx][ny] == '.' && !visited[nx][ny])
         {
             dfs(matrix, visited, nx, ny, a, b);
         }
@@ -21,8 +21,8 @@ int32_t main()
 {
     int a, b;
     cin >> a >> b;
-    vector<vector<char> > matrix(a, vector<char>(b));
-    vector<vector<bool> > visited(a, vector<bool>(b, false));
+    vector<vector<char>> matrix(a, vector<char>(b));
+    vector<vector<bool>> visited(a, vector<bool>(b, false));
     for (int i = 0; i < a; i++)
     {
         for (int j = 0; j < b; j++)
