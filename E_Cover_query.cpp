cddb1ac14287176a121668681e88d7ee92680031
farhansadeek <farhansadeek019@gmail.com>
2025-12-07 08:37:19 -0500

    Upload E_Cover_query.cpp

---
+++ E_Cover_query.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+
+int32_t main(){
+    int n, q; cin >> n >> q;
+    for(int i = 0; i < q; i++){
+        
+    }
+    return 0;
+}
