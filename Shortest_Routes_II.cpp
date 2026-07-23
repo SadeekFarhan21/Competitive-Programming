604b29626df76e2cfae587bd7b919f626fc3131f
farhansadeek <farhansadeek019@gmail.com>
2026-07-22 23:26:38 -0700

    Update files from 2026-07-22

--- Shortest_Routes_II.cpp
+++ Shortest_Routes_II.cpp
@@ -1,61 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N = 509;
-const int INF = 1e14 + 9;
-int dist[N][N];
+#define double long double
 
-int32_t main()
-{
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int n, m, q;
     cin >> n >> m >> q;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            dist[i][j] = INF;
-        }
+    vector<vector<int>> distance(n + 1, vector<int>(n + 1, 1e18));
+    for (int i = 1; i <= n; i++) {
+        distance[i][i] = 0;
     }
 
-    for (int i = 1; i <= n; i++)
-    {
-        dist[i][i] = 0;
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        distance[a][b] = min(distance[a][b], c);
+        distance[b][a] = min(distance[b][a], c);
     }
 
-    for (int i = 1; i <= m; i++)
-    {
-        int a, b, wt;
-        cin >> a >> b >> wt;
-        dist[a][b] = min(dist[a][b], wt);
-        dist[b][a] = min(dist[b][a], wt);
-    }
-
-    for (int k = 1; k <= n; k++)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if (dist[i][k] != INF && dist[k][j] != INF)
-                {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
+                distance[j][i] = min(distance[j][i], distance[j][k] + distance[k][i]);
             }
         }
     }
 
-    for (int i = 0; i < q; i++)
-    {
+    for (int i = 0; i < q; i++) {
         int a, b;
         cin >> a >> b;
-        if (dist[a][b] == INF)
-        {
-            cout << -1 << "\n";
-        }
-        else
-        {
-            cout << dist[a][b] << "\n";
-        }
+        cout << (distance[a][b] == 1e18 ? -1 : distance[a][b]) << "\n";
     }
+    cout << "\n";
     return 0;
 }
