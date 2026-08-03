4cb1ffdeec15f5b05b075bab8f492980a8c0a2da
farhansadeek <farhansadeek019@gmail.com>
2026-08-03 09:22:01 -0700

    Update files from 2026-08-03

--- Money_Sums.cpp
+++ Money_Sums.cpp
@@ -1,30 +1,43 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define double long double
 
 int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int n;
     cin >> n;
     vector<int> v(n);
-    int sum = 0;
+    int max_sum = 0;
     for (int i = 0; i < n; i++) {
         cin >> v[i];
-        sum += v[i];
+        max_sum += v[i];
     }
-
-    vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = true;
-    }
-    // each individual item to be true
+    vector<bool> dp(max_sum + 1, false);
+    dp[0] = true;
     for (int i = 0; i < n; i++) {
-        dp[i][v[i]] = true;
-    }
-    for (int i = 1; i <= sum; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i - v[j] >= 0) {
-                dp[j][i] = dp[j - 1][i] + dp[j][v[i]];
+        for (int s = max_sum; s >= v[i]; s--) {
+            if (dp[s - v[i]]) {
+                dp[s] = true;
             }
         }
     }
+    vector<int> possible_sums;
+    for (int i = 1; i <= max_sum + 1; i++) {
+        if (dp[i]) {
+            possible_sums.push_back(i);
+        }
+    }
+    cout << possible_sums.size() << '\n';
+    for (auto x : possible_sums)
+        cout << x << ' ';
+    cout << '\n';
     return 0;
 }
--- Reading_Books.cpp
+++ Reading_Books.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-int main(){
-    int n; cin >> n;
-    vector <int> v(n);
-    for(int i = 0; i < n; i++){
+int32_t main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int largest = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
         cin >> v[i];
+        largest = max(largest, v[i]);
+        sum += v[i];
     }
-    int time = v[0];
-    for(int i = 1; i < n; i++){
-        if(v[i] < v[i - 1]){
-            time += abs(v[i - 1] - v[i]);
-        }
-    }
-    cout << time << "\n";
+    cout << max(2 * largest, sum) << '\n';
     return 0;
 }
